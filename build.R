7abaf5bf4c8bca7ddb3ad0b5232cc00667742ba2
Wyatt Mosiman <wyatt@Wyatts-MacBook-Air.local>
2025-01-22 08:56:11 -0600

    create build.R

---
+++ build.R
@@ -0,0 +1,7 @@
+# this file: build.R
+# created: 2025-01-21 WM
+
+# google drive users ####
+# this identifies which google drive account to use when running functions with the googledrive package
+if(file.exists("/Users/wyatt")) driveUser <- "wdcmosiman@gmail.com" # wm laptop
+
